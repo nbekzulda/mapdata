0af1f7245f263f3154ab535fcf4eb047c5c20eb1
Nursultan <nbekzulda@gmail.com>
2019-11-21 16:56:32 +0600

    realizing current location

--- app/src/main/java/com/example/mapdata/MapsActivity.kt
+++ app/src/main/java/com/example/mapdata/MapsActivity.kt
@@ -1,18 +1,35 @@
 package com.example.mapdata
 
+import android.location.Location
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Looper
+import com.google.android.gms.location.*
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
+
+    private var latitude:Double=0.toDouble()
+    private var longitude:Double=0.toDouble()
+
+    private lateinit var mLastLocation: Location
+    private var mMarker: Marker?=null
+
+    //location
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    lateinit var locationRequest: LocationRequest
+    lateinit var locationCallback: LocationCallback
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +38,50 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        // Request runtime permission
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+            checkLocationPermission()
+        buildLocationRequest()
+        buildLocationCallBack()
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper())
+
+    }
+
+    private fun buildLocationCallBack() {
+        locationCallback = object : LocationCallback(){
+            override fun onLocationResult(p0: LocationResult?) {
+                mLastLocation = p0!!.locations.get(p0!!.locations.size - 1) // Get last location
+
+                if(mMarker != null){
+                    mMarker!!.remove()
+                }
+
+                latitude = mLastLocation.latitude
+                longitude = mLastLocation.longitude
+
+                val LatLng = LatLng(latitude, longitude)
+                val markerOptions = MarkerOptions()
+                    .position(LatLng)
+                    .title("Your position")
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))
+
+                mMarker = mMap!!.addMarker(markerOptions)
+
+
+                
+            }
+        }
+    }
+
+    private fun buildLocationRequest() {
+
+    }
+
+    private fun checkLocationPermission() {
+
     }
 
 
