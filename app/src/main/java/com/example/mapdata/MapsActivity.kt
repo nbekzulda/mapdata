4a87c87f3793002350c8b4b4208e678f9ea4d136
Nursultan <nbekzulda@gmail.com>
2019-11-21 11:10:16 +0600

    first attempt

--- app/src/main/java/com/example/mapdata/MapsActivity.kt
+++ app/src/main/java/com/example/mapdata/MapsActivity.kt
@@ -40,4 +40,6 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
+    // kotlin
+    
 }
